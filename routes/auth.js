4f25ad7fc3a219a5d2eb1ba82fa0fe419d82bc05
HasiniWijerathna <hasini@calcey.com>
2017-01-11 12:05:12 +0530

    Created a user service

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,12 @@
 'use strict';
 const express = require('express');
 const router = express.Router(); // eslint-disable-line
-const CryptoJS = require('crypto-js');
-
-const config = require('../config');
 
 const models = require('../models/index');
 
 const tokenFactory = require('../services/tokenFactory');
 const errorFactory = require('../services/errorFactory');
+const userService = require('../services/userService');
 
 /**
  * Temp object with users
@@ -39,65 +37,6 @@ const userAvailable = (username, password) => {
   return user;
 };
 
-const validateEmail = (email) => {
-  let error = null;
-
-  if (!email || email.length === 0) {
-    error = 'Invalid email';
-  } else if (email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-    error = 'Invalid email';
-  }
-
-  return error;
-}
-
-const validateName = (name) => {
-  let error = null;
-
-  if (!name || name.length === 0) {
-    error = 'Invalid name';
-  } else if (name.length < 6) {
-    error = 'Invalid name';
-  }
-
-  return error;
-}
-
-const validatePassword = (password) => {
-  let error = null;
-
-  if (!password || password.length === 0) {
-    error = 'Invalid password';
-  } else if (password.length < 6) {
-    error = 'Invalid password';
-  }
-
-  return error;
-}
-
-const validateUser = (user) => {
-  let validateUser = false;
-
-  const nameError = validateName(user.username);
-  const emailError = validateEmail(user.email);
-  const passwordError = validatePassword(user.password);
-
-  if (!nameError && !emailError && !passwordError) {
-    validateUser = true;
-  }
-
-  return validateUser;
-};
-
-/**
- * Returns the hash password
- * @param  {String} password The password
- * @return {object}          [description]
- */
-const getPasswordHash = (password) => {
-  return CryptoJS.HmacSHA256(password, config.passwordKey).toString(CryptoJS.enc.Hex);
-};
-
 /**
  * Registering the '/login' route
  */
@@ -125,8 +64,8 @@ router.post('/register', (req, res, next) => {
     password: req.body.password
   };
 
-  if (validateUser(user)) {
-    user.password = getPasswordHash(req.body.password);
+  if (userService.validateUser(user)) {
+    user.password = userService.getPasswordHash(req.body.password);
     models
       .User
       .find({
---
+++ services/userService.js
@@ -0,0 +1,65 @@
+'use strict';
+const CryptoJS = require('crypto-js');
+
+const config = require('../config');
+
+const validateEmail = (email) => {
+  let error = null;
+
+  if (!email || email.length === 0) {
+    error = 'Invalid email';
+  } else if (email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+    error = 'Invalid email';
+  }
+
+  return error;
+}
+
+const validateName = (name) => {
+  let error = null;
+
+  if (!name || name.length === 0) {
+    error = 'Invalid name';
+  } else if (name.length < 6) {
+    error = 'Invalid name';
+  }
+
+  return error;
+}
+
+const validatePassword = (password) => {
+  let error = null;
+
+  if (!password || password.length === 0) {
+    error = 'Invalid password';
+  } else if (password.length < 6) {
+    error = 'Invalid password';
+  }
+
+  return error;
+}
+
+const validateUser = (user) => {
+  let validateUser = false;
+
+  const nameError = validateName(user.username);
+  const emailError = validateEmail(user.email);
+  const passwordError = validatePassword(user.password);
+
+  if (!nameError && !emailError && !passwordError) {
+    validateUser = true;
+  }
+
+  return validateUser;
+};
+
+/**
+ * Returns the hash password
+ * @param  {String} password The password
+ * @return {object}          [description]
+ */
+const getPasswordHash = (password) => {
+  return CryptoJS.HmacSHA256(password, config.passwordKey).toString(CryptoJS.enc.Hex);
+};
+
+module.exports = { validateUser, getPasswordHash };
