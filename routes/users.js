63fb72a11810d931c039220a3fa45aa994ef30e5
HasiniWijerathna <hasini@calcey.com>
2017-02-08 11:44:05 +0530

    Added a users route

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,33 @@ const express = require('express');
 const router = express.Router(); // eslint-disable-line
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  const users = {
-    users: [{
-      username: 'mthahzan',
-    }, {
-      username: 'mnhaqeel',
-    }],
-  };
+router.put('/', (req, res) => {
+  // Update user with ID 'userId'
+  models
+  .User
+  .find({
+    where: {
+      id: req.body.id,
+    },
+  })
+  .then((user) => {
+    let promise = null;
 
-  res.send(users);
-});
-
-router.get('/get', (req, res) => {
-  res.send('GET request ');
+    if(user) {
+      promise = user.updateAttributes({
+       name: req.body.name,
+       email: req.body.email,
+       password: req.body.password,
+      });
+    } else {
+      throw errorFactory.badRequest(req, 'Invalied User');
+    }
+    return promise;
+  })
+  .then((updatedUser) => {
+    res.send(updatedUser);
+  })
+  .catch(next);
 });
 
 module.exports = router;
